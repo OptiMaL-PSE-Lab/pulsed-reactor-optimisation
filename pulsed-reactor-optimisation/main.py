0eb86afc18f3c76a72759c78111299e752f9dae2
Tom Savage <tomsavage@macbook-pro-tom.local>
2021-09-16 20:23:12 +0100

    main: added function to generate suggested point and store in table

--- pulsed-reactor-optimisation/main.py
+++ pulsed-reactor-optimisation/main.py
@@ -1,45 +1,60 @@
 from bayes_opt import BayesianOptimization, UtilityFunction
-import numpy as np
+import pandas as pd
 from utils import read_data, discretise_design, CFD_function
 
-data = read_data()  # reading data from csv
-inputs = data[
-    ["Design", "Dt (mm)", "Rc (mm)", "P (mm)", "V (mL)", "Ren", "f (Hz)", "xo (mm)"]
-]  # separating input columns
-outputs = data[["Navg"]]  #  obtaining output column
-inputs = inputs.to_dict(orient="records")  # producing a list of dictionaries
-outputs = outputs.values[:, 0]  # producing a list of outputs
 
-# defining an aquisition function
-utility = UtilityFunction(kind="ucb", kappa=2.5, xi=0.0)
-# setting up the optimisation problem
-optimizer = BayesianOptimization(
-    f=CFD_function,
-    pbounds={
-        "Design": (1, 6),
-        "Dt (mm)": (5, 7.5),
-        "Rc (mm)": (12.5, 32.5),
-        "P (mm)": (7.5, 12.5),
-        "V (mL)": (16, 18),
-        "Ren": (10, 50),
-        "f (Hz)": (0, 10),
-        "xo (mm)": (0, 10),
-    },
-    verbose=2,
-    random_state=1,
-)
+def variable_suggestion():
+    data = read_data()  # reading data from csv
+    generated_data = pd.read_csv("data/helical_coil_data_generated.csv")
+    var_list = [
+        "Design",
+        "Dt (mm)",
+        "Rc (mm)",
+        "P (mm)",
+        "V (mL)",
+        "Ren",
+        "f (Hz)",
+        "xo (mm)",
+    ]
+    inputs = data[var_list]  # separating input columns
+    generated_inputs = generated_data[var_list]
+    outputs = data[["Navg"]]  #  obtaining output column
+    generated_outputs = generated_data[["Navg"]]
+    inputs = inputs.append(generated_inputs)
+    outputs = outputs.append(generated_outputs)
+    inputs = inputs.to_dict(orient="records")  # producing a list of dictionaries
+    outputs = outputs.values[:, 0]  # producing a list of outputs
 
-# initialising Gaussian processes with existing data
-for i in range(len(outputs)):
-    optimizer.register(params=inputs[i], target=outputs[i])
+    # defining an aquisition function
+    utility = UtilityFunction(kind="ucb", kappa=2.5, xi=0.0)
+    # setting up the optimisation problem
+    optimizer = BayesianOptimization(
+        f=CFD_function,
+        pbounds={
+            "Design": (1, 6),
+            "Dt (mm)": (5, 7.5),
+            "Rc (mm)": (12.5, 32.5),
+            "P (mm)": (7.5, 12.5),
+            "V (mL)": (16, 18),
+            "Ren": (10, 50),
+            "f (Hz)": (0, 10),
+            "xo (mm)": (0, 10),
+        },
+        verbose=2,
+        random_state=1,
+    )
 
-while True:
-    # obtaining the next suggested point
+    # initialising Gaussian processes with existing data
+    for i in range(len(outputs)):
+        optimizer.register(params=inputs[i], target=outputs[i])
+
     next_point = optimizer.suggest(utility)
-    # discretising the design variable
     next_point = discretise_design(next_point)
-    # evaluating the set of inputs
-    output = CFD_function(next_point)
-    print("Current objective function: ", np.round(output, 3))
-    # adding the point and its output to the GP
-    optimizer.register(params=next_point, target=output)
+    print(next_point)
+    df = pd.DataFrame(next_point, index=[0])
+    generated_data = generated_data.append(df)
+    generated_data.to_csv("data/helical_coil_data_generated.csv", index=False)
+    return
+
+
+variable_suggestion()
--- pulsed-reactor-optimisation/utils.py
+++ pulsed-reactor-optimisation/utils.py
@@ -1,5 +1,5 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 
 
 def read_data():
