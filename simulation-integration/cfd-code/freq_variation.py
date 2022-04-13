e28454850184b33856de3a4be2ced1af348dc7ab
Tom Savage <t.savage@imperial.ac.uk>
2022-04-13 10:12:53 +0000

    updated N value estimation

--- simulation-integration/cfd-code/freq_variation.py
+++ simulation-integration/cfd-code/freq_variation.py
@@ -1,17 +1,16 @@
 import os
-import math
 from os import path
 from distutils.dir_util import copy_tree
+from re import I
 from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
 from PyFoam.LogAnalysis.BoundingLogAnalyzer import BoundingLogAnalyzer
 from PyFoam.Execution.AnalyzedRunner import AnalyzedRunner
 from PyFoam.LogAnalysis.SimpleLineAnalyzer import GeneralSimpleLineAnalyzer
 import numpy as np
 from scipy.signal import find_peaks
-from numpy import linspace
-
-amp1 = linspace(0.002, 0.006, 3)
-
+from scipy.optimize import minimize
+from scipy.special import factorial
+import matplotlib.pyplot as plt
 
 class CompactAnalyzer(BoundingLogAnalyzer):
     def __init__(self):
@@ -26,7 +25,8 @@ class CompactAnalyzer(BoundingLogAnalyzer):
 
 solver = "pimpleFoam"
 
-for a in amp1:
+for a in [0.003]:
+
     os.mkdir("pimple-amp%.3f" % a)
     os.mkdir("pimple-amp%.3f/0" % a)
     os.mkdir("pimple-amp%.3f/constant" % a)
@@ -56,7 +56,7 @@ for a in amp1:
         argv=[solver, "-case", Newcase],
         logname="Solution",
     )
-    print
+
     run.start()
 
     times = np.array(run.getAnalyzer("concentration").lines.getTimes())
@@ -66,30 +66,43 @@ for a in amp1:
 
     # times and values are numpy arrays, you can do whatever you want here
 
-    time = np.array(times)
-    value = np.array(values)
-    
-    peaks, _ = find_peaks(value, prominence=0.001)
-    print(_)
-    times_peaks = time[peaks]
-    values_peaks = value[peaks]
+    time = np.array(times) # list of times
+    value = np.array(values) # list of concentrations 
 
-    deltaT = np.diff(times_peaks)
-    et = values_peaks / (sum(values_peaks * deltaT))
-    tau = (sum(times_peaks * values_peaks * deltaT)) / sum(values_peaks * deltaT)
+    # obtaining a smooth curve by taking peaks  
+    peaks, _ = find_peaks(value, prominence=0.001) 
+    times = time[peaks]
+    values = value[peaks]
 
-    eteeta = tau * et
-    teeta = times_peaks / tau
+    # difference between time values
+    dt = np.diff(times)[0]
 
-    min = 0
-    for N in range(30, 35, 1):
-        xy = (N * ((N * teeta) ** (N - 1))) * (np.exp(-N * teeta))
-        z = math.factorial(N - 1)
-        eteeta_calc = xy / z
+    # getting lists of interest (theta, e_theta)
+    et = values / (sum(values * dt))
+    tau = (sum(times * values  * dt)) / sum(values * dt)
+    etheta = tau * et
+    theta = times / tau
 
-        error_sq = sum(eteeta_calc - eteeta)
+    def calc_etheta(N,theta):
+        z = factorial(N-1)
+        xy = (N * ((N * theta) ** (N - 1))) * (np.exp(-N * theta))
+        etheta_calc = xy / z
+        return etheta_calc
 
-        if error_sq < min:
-            min = error_sq
-            N_value = N
-    print(min, N)
+    def loss(N,theta,etheta):
+        for i in range(len(theta)):
+            etheta_calc = calc_etheta(N,theta[i])
+            error_sq = (etheta_calc - etheta[i])**2
+        return error_sq
+
+    N = minimize(loss,x0=35,bounds=((0.1,1000),),args=(theta,etheta)).x
+
+    plt.figure()
+    plt.plot(theta,etheta,c='k',linestyle='dashed',label='CFD')
+    etheta_calc = [] 
+    for t in theta:
+        etheta_calc.append(calc_etheta(N,t))
+    plt.plot(theta,etheta_calc,c='k',label='Dimensionless')
+    plt.grid()
+    plt.legend()
+    plt.savefig(str(a)+'.png')
