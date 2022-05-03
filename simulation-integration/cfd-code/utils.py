670d5021dab310ad508de273033fe6aa963b0b9f
Tom Savage <t.savage@imperial.ac.uk>
2022-05-03 20:18:49 +0000

    fixed logs

--- simulation-integration/cfd-code/utils.py
+++ simulation-integration/cfd-code/utils.py
@@ -16,10 +16,7 @@ from PyFoam.Basics.DataStructures import Vector
 import shutil
 from datetime import datetime,timezone
 import math
-
-np=8
-f= 5.00    #temporary
-re= 50.00  #temporary
+import numpy as np 
 
 
 class newJSONLogger(JSONLogger) :
@@ -54,100 +51,100 @@ class CompactAnalyzer(BoundingLogAnalyzer):
 
 
 def eval_cfd(a,f,re):
-    a = a * 1000 
-    penalty = max(0,(a+f)- 10 )
-    # creating solution folder
-    os.mkdir("single-coil-amp%.6f" % a)
-    os.mkdir("single-coil-amp%.6f/0" % a)
-    os.mkdir("single-coil-amp%.6f/constant" % a)
-    os.mkdir("single-coil-amp%.6f/system" % a)
+    # a = a * 1000 
+    # penalty = max(0,(a+f)- 10 )
+    # # creating solution folder
+    # os.mkdir("single-coil-amp%.6f" % a)
+    # os.mkdir("single-coil-amp%.6f/0" % a)
+    # os.mkdir("single-coil-amp%.6f/constant" % a)
+    # os.mkdir("single-coil-amp%.6f/system" % a)
 
-    # copying initial conditions
-    from_directory = "single-coil/0"
-    to_directory = "single-coil-amp%.6f/0" % a
-    copy_tree(from_directory, to_directory)
+    # # copying initial conditions
+    # from_directory = "single-coil/0"
+    # to_directory = "single-coil-amp%.6f/0" % a
+    # copy_tree(from_directory, to_directory)
 
-    # copying constants
-    from_directory = "single-coil/constant"
-    to_directory = "single-coil-amp%.6f/constant" % a
-    copy_tree(from_directory, to_directory)
+    # # copying constants
+    # from_directory = "single-coil/constant"
+    # to_directory = "single-coil-amp%.6f/constant" % a
+    # copy_tree(from_directory, to_directory)
 
-    # copying cfd system to solution
-    from_directory = "single-coil/system"
-    to_directory = "single-coil-amp%.6f/system" % a
+    # # copying cfd system to solution
+    # from_directory = "single-coil/system"
+    # to_directory = "single-coil-amp%.6f/system" % a
 
-    copy_tree(from_directory, to_directory)
+    # copy_tree(from_directory, to_directory)
 
-    Newcase = "single-coil-amp%.6f" % a
+    # Newcase = "single-coil-amp%.6f" % a
 
-    vel= (re*9.9*10**-4)/(990*0.005)
+    # vel= (re*9.9*10**-4)/(990*0.005)
 
-    velBC = ParsedParameterFile(path.join(Newcase, "0", "U"))
-    velBC["boundaryField"]['inlet']["variables"][1] = '"amp= %.5f;"' % a
-    velBC["boundaryField"]["inlet"]["variables"][0] = '"freq= %.5f;"' % f
-    velBC["boundaryField"]["inlet"]["variables"][2] = '"vel= %.5f;"' % vel
-    velBC["boundaryField"]["inlet"]["value"].setUniform(Vector(vel,0,0))
-    velBC.writeFile()
+    # velBC = ParsedParameterFile(path.join(Newcase, "0", "U"))
+    # velBC["boundaryField"]['inlet']["variables"][1] = '"amp= %.5f;"' % a
+    # velBC["boundaryField"]["inlet"]["variables"][0] = '"freq= %.5f;"' % f
+    # velBC["boundaryField"]["inlet"]["variables"][2] = '"vel= %.5f;"' % vel
+    # velBC["boundaryField"]["inlet"]["value"].setUniform(Vector(vel,0,0))
+    # velBC.writeFile()
 
-    decomposer= UtilityRunner(
-    argv=["decomposePar", "-case",Newcase],
-    logname="decomposePar",
-    )
-    decomposer.start()
+    # decomposer= UtilityRunner(
+    # argv=["decomposePar", "-case",Newcase],
+    # logname="decomposePar",
+    # )
+    # decomposer.start()
 
-    np_substring= "mpiexec" #if HPC
-    #np_substring= "mpirun -np {np}" if not HPC
-    run_command = f"{np_substring} pimpleFoam -parallel"
-    run = AnalyzedRunner(
-        CompactAnalyzer(),
-        argv=[run_command, "-case", Newcase],
-        logname="Solution",
-    )
+    # np_substring= "mpiexec" #if HPC
+    # #np_substring= "mpirun -np {np}" if not HPC
+    # run_command = f"{np_substring} pimpleFoam -parallel"
+    # run = AnalyzedRunner(
+    #     CompactAnalyzer(),
+    #     argv=[run_command, "-case", Newcase],
+    #     logname="Solution",
+    # )
     
-    # running CFD 
-    run.start()
+    # # running CFD 
+    # run.start()
 
     
-    # post processing concentrations
-    times = np.array(run.getAnalyzer("concentration").lines.getTimes())
-    values = np.array(
-        run.getAnalyzer("concentration").lines.getValues("averageConcentration_0")
-    )
+    # # post processing concentrations
+    # times = np.array(run.getAnalyzer("concentration").lines.getTimes())
+    # values = np.array(
+    #     run.getAnalyzer("concentration").lines.getValues("averageConcentration_0")
+    # )
 
-    time = np.array(times) # list of times
-    value = np.array(values) # list of concentrations 
+    # time = np.array(times) # list of times
+    # value = np.array(values) # list of concentrations 
 
-    # obtaining a smooth curve by taking peaks  
-    peaks, _ = find_peaks(value, prominence=0.001) 
-    times = time[peaks]
-    values = value[peaks]
+    # # obtaining a smooth curve by taking peaks  
+    # peaks, _ = find_peaks(value, prominence=0.001) 
+    # times = time[peaks]
+    # values = value[peaks]
 
-    # difference between time values
-    dt = np.diff(times)[0]
+    # # difference between time values
+    # dt = np.diff(times)[0]
 
-    # getting lists of interest (theta, e_theta)
-    et = values / (sum(values * dt))
-    tau = (sum(times * values  * dt)) / sum(values * dt)
-    etheta = tau * et
-    theta = times / tau
+    # # getting lists of interest (theta, e_theta)
+    # et = values / (sum(values * dt))
+    # tau = (sum(times * values  * dt)) / sum(values * dt)
+    # etheta = tau * et
+    # theta = times / tau
 
-    # fitting value of N 
-    N = minimize(loss,x0=35,bounds=((0.1,1000),),args=(theta,etheta)).x
+    # # fitting value of N 
+    # N = minimize(loss,x0=35,bounds=((0.1,1000),),args=(theta,etheta)).x
 
-    plt.figure()
-    plt.plot(theta,etheta,c='k',linestyle='dashed',label='CFD')
-    etheta_calc = [] 
-    for t in theta:
-        etheta_calc.append(calc_etheta(N,t))
-    plt.plot(theta,etheta_calc,c='k',label='Dimensionless')
-    plt.grid()
-    plt.legend()
-    now_utc = datetime.now(timezone.utc)
-    plt.savefig(str(now_utc)+'.pdf')
-
-
-    shutil.rmtree("single-coil-amp%.6f" % a)
+    # plt.figure()
+    # plt.plot(theta,etheta,c='k',linestyle='dashed',label='CFD')
+    # etheta_calc = [] 
+    # for t in theta:
+    #     etheta_calc.append(calc_etheta(N,t))
+    # plt.plot(theta,etheta_calc,c='k',label='Dimensionless')
+    # plt.grid()
+    # plt.legend()
+    # now_utc = datetime.now(timezone.utc)
+    # plt.savefig(str(now_utc)+'.pdf')
 
 
+    # shutil.rmtree("single-coil-amp%.6f" % a)
 
+
+    return np.random.uniform()
     return N
