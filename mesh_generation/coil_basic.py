2d780811f96eb2bcc872566beea511915c13d454
Tom Savage <tom@crvstech.com>
2022-07-18 11:37:53 +0000

    major refactoring

--- mesh_generation/coil_basic.py
+++ mesh_generation/coil_basic.py
@@ -1,7 +1,10 @@
 import numpy as np
-from classy_blocks.classes.primitives import Edge
-from classy_blocks.classes.block import Block
-from classy_blocks.classes.mesh import Mesh
+import sys
+import os
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from mesh_generation.classy_blocks.classes.primitives import Edge
+from mesh_generation.classy_blocks.classes.block import Block
+from mesh_generation.classy_blocks.classes.mesh import Mesh
 import shutil
 import os
 import matplotlib.pyplot as plt
@@ -54,6 +57,7 @@ def create_circle(d,flip):
     x, y, z = rotate_z(x, y, z, t)
     x += c_x
     y += c_y
+    x, y, z = rotate_z(x, y, z, np.pi/2)
     return x, y, z
 
 
@@ -260,23 +264,26 @@ def create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, path):
     )
     ax.set_title('Pitch: '+str(np.round(pitch,2))+', Coil Radius: '+str(np.round(coil_rad,2))+', Inversion %: '+str(np.round(il,2)*100))
     #plt.show()
-    plt.savefig("output_images/"+path+".png", dpi=400)
+    plt.savefig(path+"/pre-render.png", dpi=400)
     try:
-        shutil.copytree("base", path)
+        shutil.copytree("mesh_generation/base", path)
     except:
         print("file already exists...")
 
     # run script to create mesh
     mesh.write(output_path=os.path.join(path, "system", "blockMeshDict"), geometry=None)
-    os.system(path + "/Allrun.mesh")
+    with open(os.path.join(path,"system", "blockMeshDict"),'r') as file:
+         filedata= file.read()
+    filedata = filedata.replace('scale   1','scale   0.01')
+    with open(os.path.join(path,"system", "blockMeshDict"),'w') as file:
+         file.write(filedata)
+    os.system(path +"/Allrun.mesh")
+    return 
 
+# tube_rad = 0.5
+# length = 60
+# coil_rad = 3
+# pitch = 3
+# inversion_loc = 0.5
 
-tube_rad = 0.5
-length = 60
-
-coil_rad = 3
-pitch = 3
-inversion_loc = 0.5
-
-
-create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, path='coil_basic')
+# create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, path='coil_basic')
