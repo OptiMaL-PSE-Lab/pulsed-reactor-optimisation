f632029ea696e1e487de3339fa4426278f5625dc
Tom Savage <t.savage@imperial.ac.uk>
2022-07-18 11:37:53 +0000

    major refactoring

---
+++ simulation-integration/main_oc.py
@@ -0,0 +1,45 @@
+from bayes_opt_with_constraints.bayes_opt import BayesianOptimization, UtilityFunction
+from utils import eval_cfd_operating_conditions, newJSONLogger
+from bayes_opt.event import Events
+import json
+
+logger = newJSONLogger(path="/output/logs_oc.json")
+
+# defining utility function
+utility = UtilityFunction(kind="ucb", kappa=2.5, xi=0.0)
+
+
+def pcon(x):
+    return 1000000 - (x[0] * 1000 + x[1])
+
+
+pcon_dict = {"type": "ineq", "fun": pcon}
+
+# setting up the optimisation problem
+optimizer = BayesianOptimization(
+    f=eval_cfd_operating_conditions,
+    pbounds={"a": (0.001, 0.008), "f": (2, 8), "re": (10, 50)},
+    pcons=pcon_dict,
+    verbose=2,
+    random_state=1,
+)
+
+
+# Opening JSON file
+try:
+    logs = []
+    with open("simulation-integration/output/logs_oc.json") as f:
+        for line in f:
+            logs.append(json.loads(line))
+
+    for log in logs:
+        optimizer.register(params=log["params"], target=log["target"])
+except FileNotFoundError:
+    pass
+# assign logger to optimizer
+optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
+
+while True:
+    next_point = optimizer.suggest(utility)
+    target = eval_cfd_operating_conditions(**next_point)
+    optimizer.register(params=next_point, target=target)
--- simulation-integration/utils.py
+++ simulation-integration/utils.py
@@ -1,21 +1,24 @@
 import numpy as np
 from scipy.special import factorial
-from PyFoam.LogAnalysis.SimpleLineAnalyzer import GeneralSimpleLineAnalyzer
-from PyFoam.LogAnalysis.BoundingLogAnalyzer import BoundingLogAnalyzer
 import matplotlib.pyplot as plt
 import os
-import pickle
 from os import path
+import shutil
+import sys
+from uuid import uuid4
+import pickle
 from distutils.dir_util import copy_tree
+from scipy.signal import find_peaks
+from bayes_opt.logger import JSONLogger
+from PyFoam.Basics.DataStructures import Vector
 from PyFoam.RunDictionary.ParsedParameterFile import ParsedParameterFile
 from PyFoam.Execution.AnalyzedRunner import AnalyzedRunner
 from PyFoam.Execution.UtilityRunner import UtilityRunner
-from scipy.signal import find_peaks
-from scipy.optimize import minimize
-from bayes_opt.logger import JSONLogger
-from PyFoam.Basics.DataStructures import Vector
-import shutil
-from coil_basic import create_mesh
+from PyFoam.LogAnalysis.SimpleLineAnalyzer import GeneralSimpleLineAnalyzer
+from PyFoam.LogAnalysis.BoundingLogAnalyzer import BoundingLogAnalyzer
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from mesh_generation.coil_basic import create_mesh
 
 class newJSONLogger(JSONLogger):
     def __init__(self, path):
@@ -54,30 +57,27 @@ class CompactAnalyzer(BoundingLogAnalyzer):
         )
 
 
-def eval_cfd(a, f, re):
-    #TODO: geomteric parameters should be received as arguments, temporarily initialised 
+def eval_cfd(a, f, re,coil_rad,pitch):
     tube_rad = 0.5
     length = 60
-    coil_rad = 3
-    pitch = 3
-    inversion_loc = 0.5
+    inversion_loc = None
 
     #creating solution folder and copying folders
-    filepath= "output/single-coil-a%.6f-coilrad%.3f" % (a,coil_rad)
-    hostpath= "output/single-coil"
+    filepath= "simulation-integration/output/"+str(uuid4())
+    hostpath= "mesh_generation/base"
     os.mkdir(filepath)
     shutil.copy((os.path.join(hostpath,"Allrun.mesh")),filepath)
     sub_folders= ['0','constant','system']
     for i in sub_folders:
         subpath= os.path.join(filepath,i)
         os.mkdir(subpath)
-        from_directory = os.path.join (hostpath,i)
+        from_directory = os.path.join(hostpath,i)
         to_directory = subpath
         copy_tree(from_directory, to_directory)
 
-    Newcase= filepath
-    create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, path=Newcase)
-    os.system(Newcase + "/Allrun.mesh")
+    newcase= filepath
+    create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, path=newcase)
+
     vel = (re * 9.9 * 10 ** -4) / (990 * 0.005)
 
     print('\n a: ',a,'\n')
@@ -85,7 +85,7 @@ def eval_cfd(a, f, re):
     print('Re: ',re,'\n')
     print('vel: ',vel,'\n')
 
-    velBC = ParsedParameterFile(path.join(Newcase, "0", "U"))
+    velBC = ParsedParameterFile(path.join(newcase, "0", "U"))
     velBC["boundaryField"]["inlet"]["variables"][1] = '"amp= %.5f;"' % a
     velBC["boundaryField"]["inlet"]["variables"][0] = '"freq= %.5f;"' % f
     velBC["boundaryField"]["inlet"]["variables"][2] = '"vel= %.5f;"' % vel
@@ -93,7 +93,7 @@ def eval_cfd(a, f, re):
     velBC.writeFile()
 
     decomposer = UtilityRunner(
-        argv=["decomposePar", "-case", Newcase],
+        argv=["decomposePar", "-case", newcase],
         logname="decomposePar",
     )
     decomposer.start()
@@ -103,7 +103,7 @@ def eval_cfd(a, f, re):
     run_command = f"{np_substring} pimpleFoam -parallel"
     run = AnalyzedRunner(
         CompactAnalyzer(),
-        argv=[run_command, "-case", Newcase],
+        argv=[run_command, "-case", newcase],
         logname="Solution",
     )
 
@@ -138,7 +138,7 @@ def eval_cfd(a, f, re):
     plt.grid()
     plt.xlabel("time")
     plt.ylabel("concentration")
-    plt.savefig("output/preprocessed_plot.pdf")
+    plt.savefig("simulation-integration/output/preprocessed_plot.pdf")
 
     # difference between time values
     dt = np.diff(times_peaks)[0]
@@ -170,7 +170,121 @@ def eval_cfd(a, f, re):
     plt.plot(theta, etheta_calc, c="k", label="Dimensionless")
     plt.grid()
     plt.legend()
-    plt.savefig("output/dimensionless_conversion.pdf")
+    plt.savefig("simulation-integration/output/dimensionless_conversion.pdf")
+
+
+    return N
+
+def eval_cfd_operating_conditions(a, f, re):
+
+    #creating solution folder and copying folders
+    filepath= "simulation-integration/output/"+str(uuid4())
+    hostpath= "mesh_generation/single-coil"
+    os.mkdir(filepath)
+    
+    sub_folders= ['0','constant','system']
+    for i in sub_folders:
+        subpath= os.path.join(filepath,i)
+        os.mkdir(subpath)
+        from_directory = os.path.join(hostpath,i)
+        to_directory = subpath
+        copy_tree(from_directory, to_directory)
+
+    newcase = filepath
+
+    vel = (re * 9.9 * 10 ** -4) / (990 * 0.005)
+
+    print('\n a: ',a,'\n')
+    print('f: ',f,'\n')
+    print('Re: ',re,'\n')
+    print('vel: ',vel,'\n')
+
+    velBC = ParsedParameterFile(path.join(newcase, "0", "U"))
+    velBC["boundaryField"]["inlet"]["variables"][1] = '"amp= %.5f;"' % a
+    velBC["boundaryField"]["inlet"]["variables"][0] = '"freq= %.5f;"' % f
+    velBC["boundaryField"]["inlet"]["variables"][2] = '"vel= %.5f;"' % vel
+    velBC["boundaryField"]["inlet"]["value"].setUniform(Vector(vel, 0, 0))
+    velBC.writeFile()
+
+    decomposer = UtilityRunner(
+        argv=["decomposePar", "-case", newcase],
+        logname="decomposePar",
+    )
+    decomposer.start()
+
+    np_substring = "mpiexec"  # if HPC
+    # np_substring= "mpirun -np {np}" if not HPC
+    run_command = f"{np_substring} pimpleFoam -parallel"
+    run = AnalyzedRunner(
+        CompactAnalyzer(),
+        argv=[run_command, "-case", newcase],
+        logname="Solution",
+    )
+
+    # running CFD
+    run.start()
+
+    # post processing concentrations
+    times = run.getAnalyzer("concentration").lines.getTimes()
+    values = run.getAnalyzer("concentration").lines.getValues("averageConcentration_0")
+
+    time = np.array(times)  # list of times
+    value = np.array(values)  # list of concentrations
+
+
+    with open("output/results.pickle", "wb") as handle:
+        pickle.dump({"t": time, "c": value}, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # with open("output/results.pickle", "rb") as handle:
+    #     res = pickle.load(handle)
+    
+    # time = res['t']
+    # value = res['c']
+
+    # obtaining a smooth curve by taking peaks
+    peaks, _ = find_peaks(value, prominence=0.000001)
+    times_peaks = time[peaks]
+    values_peaks = value[peaks]
+
+    plt.figure()
+    plt.plot(time, value, c="k", lw=1, alpha=0.1)
+    plt.plot(times_peaks, values_peaks, c="r", lw=1)
+    plt.grid()
+    plt.xlabel("time")
+    plt.ylabel("concentration")
+    plt.savefig("simulation-integration/output/preprocessed_plot.pdf")
+
+    # difference between time values
+    dt = np.diff(times_peaks)[0]
+
+    # getting lists of interest (theta, e_theta)
+    et = values_peaks / (sum(values_peaks * dt))
+    tau = (sum(times_peaks * values_peaks * dt)) / sum(values_peaks * dt)
+    etheta = tau * et
+    theta = times_peaks / tau
+
+    # fitting value of N
+    s = 10000
+    x0_list = np.array([np.logspace(np.log(1),np.log(50), s),np.random.uniform(-1,0, s),np.random.uniform(0,1, s)]).T
+
+    best = np.Inf
+    for x0 in x0_list:
+        l = loss(x0,theta,etheta)
+        if l < best:
+            best = l
+            X = x0
+
+    N,off,up = X 
+
+    plt.figure()
+    plt.plot(theta, etheta, c="k", linestyle="dashed", label="CFD")
+    etheta_calc = []
+    for t in theta:
+        etheta_calc.append(calc_etheta(N, t,off,up))
+    plt.plot(theta, etheta_calc, c="k", label="Dimensionless")
+    plt.grid()
+    plt.legend()
+    plt.savefig("simulation-integration/output/dimensionless_conversion.pdf")
 
 
     return N
