f2609f8a16ed028e1d6b02cf90513c0a3b948d2d
Tom Savage <t.savage@imperial.ac.uk>
2022-07-29 08:20:54 +0000

    added slightly more refined topology

--- mesh_generation/coil_basic.py
+++ mesh_generation/coil_basic.py
@@ -87,7 +87,7 @@ def create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, path):
     h = coils * pitch 
     keys = ["x", "y", "t","t_x", "r", "z"]
     data = {}
-    points = 221 # points determined by length
+    points = 161 # points determined by length
     t_x = -np.arctan(h/length)
     if inversion_loc is None:
         il = 1
@@ -277,9 +277,9 @@ def create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, path):
                 block.set_patch("bottom", "outlet")
 
 
-            block.chop(0, count=5)
-            block.chop(1, count=5)
-            block.chop(2, count=2)
+            block.chop(0, count=4)
+            block.chop(1, count=4)
+            block.chop(2, count=1)
 
             mesh.add_block(block)
 
@@ -329,9 +329,9 @@ def create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, path):
                 block.set_patch("bottom", "outlet")
 
 
-            block.chop(0, count=5)
-            block.chop(1, count=5)
-            block.chop(2, count=2)
+            block.chop(0, count=4)
+            block.chop(1, count=4)
+            block.chop(2, count=1)
 
             mesh.add_block(block)
 
@@ -366,9 +366,9 @@ def create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, path):
                 block.set_patch("bottom", "outlet")
 
 
-            block.chop(0, count=5)
-            block.chop(1, count=5)
-            block.chop(2, count=2)
+            block.chop(0, count=4)
+            block.chop(1, count=4)
+            block.chop(2, count=1)
 
             mesh.add_block(block)
         # copy template folder
