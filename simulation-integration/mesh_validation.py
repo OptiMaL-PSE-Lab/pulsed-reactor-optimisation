8f2a8a31c45311a7351777c9f5e0e7e8537d4068
Tom Savage <t.savage@imperial.ac.uk>
2022-08-02 12:59:32 +0000

    fixed range bug

--- simulation-integration/mesh_validation.py
+++ simulation-integration/mesh_validation.py
@@ -1,7 +1,7 @@
 from utils import eval_cfd_validation
 import pandas as pd 
 
-for f in range[1,2,3,4]:
+for f in [1,2,3,4]:
     N,time,value = eval_cfd_validation(0.002,5,50,0.012,0.01,0.0025,0.0753,f)
     res = pd.DataFrame({'time':time,'concentration':value})
     res.to_csv('simulation-integration/output_validation/e_1_fid_1'+str(f)+'.csv')
