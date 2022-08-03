607d5ddd81b744384174deda5ed13eb9f8f19be0
Tom Savage <t.savage@imperial.ac.uk>
2022-08-03 15:52:01 +0000

    updated script

--- simulation-integration/mesh_validation.py
+++ simulation-integration/mesh_validation.py
@@ -13,7 +13,7 @@ e1 = exp['e1'][exp['e1'].isnull()==False].values
 e2 = exp['e2'][exp['e2'].isnull()==False].values
 
 fig,ax = plt.subplots(1,2,figsize=(8,4))
-plt.subplots_adjust(wspace=0.3)
+fig.subplots_adjust(wspace=0.3)
 for i in range(2):
     ax[i].set_xlabel(r'$\theta$')
     ax[i].set_ylabel(r'$E({\theta})$')
@@ -22,7 +22,7 @@ for i in range(2):
 ax[0].scatter(t1,e1,c='k',s=10,marker='x',lw=0.5)
 ax[1].scatter(t2,e2,c='k',s=10,marker='x',lw=0.5,label='Experiment')
 
-plt.savefig('simulation-integration/output_validation/experimental_validation.png',dpi=800)
+fig.savefig('simulation-integration/output_validation/experimental_validation.png',dpi=800)
 
 color = iter(cm.viridis(np.linspace(0, 1, 5)))
 
@@ -31,15 +31,19 @@ for f in [2,3,4,5,6]:
     N,time,value,path = eval_cfd_validation(0.002,5,50,0.012,0.01,0.0025,0.0753,f)
     theta,etheta = val_to_rtd(time,value,path)
     ax[0].plot(theta,etheta,c=c,alpha=0.75)
-    plt.savefig('simulation-integration/output_validation/experimental_validation.png',dpi=800)
-    res = pd.DataFrame({'time':time,'concentration':value,'theta':theta,'etheta':etheta})
+    fig.savefig('simulation-integration/output_validation/experimental_validation.png',dpi=800)
+    res1 = pd.DataFrame({'time':time,'concentration':value})
+    res2 = pd.DataFrame({'theta':theta,'etheta':etheta})
+    res = pd.concat([res1, res2], axis=1) 
     res.to_csv('simulation-integration/output_validation/e_1_fid_'+str(f)+'.csv')
 
     N,time,value,path = eval_cfd_validation(0.004,5,50,0.012,0.01,0.0025,0.0753,f)
     theta,etheta = val_to_rtd(time,value,path)
     ax[1].plot(theta,etheta,c=c,alpha=0.75,label='Fidelity: '+str(f))
-    plt.savefig('simulation-integration/output_validation/experimental_validation.png',dpi=800)
-    res = pd.DataFrame({'time':time,'concentration':value,'theta':theta,'etheta':etheta})
+    fig.savefig('simulation-integration/output_validation/experimental_validation.png',dpi=800)
+    res1 = pd.DataFrame({'time':time,'concentration':value})
+    res2 = pd.DataFrame({'theta':theta,'etheta':etheta})
+    res = pd.concat([res1, res2], axis=1) 
     res.to_csv('simulation-integration/output_validation/e_2_fid_'+str(f)+'.csv')
 ax[1].legend()
-plt.savefig('simulation-integration/output_validation/experimental_validation.png',dpi=800)
+fig.savefig('simulation-integration/output_validation/experimental_validation.png',dpi=800)
