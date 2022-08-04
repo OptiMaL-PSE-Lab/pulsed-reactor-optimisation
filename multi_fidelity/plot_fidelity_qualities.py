9963c143511de5c6f724585e1509dadab34ad2e8
Tom Savage <t.savage@imperial.ac.uk>
2022-08-04 21:51:56 +0000

    updated fidelity plot

--- multi_fidelity/plot_fidelity_qualities.py
+++ multi_fidelity/plot_fidelity_qualities.py
@@ -8,16 +8,16 @@ def plot_fidelities(M):
 	f2 = [0,0.25,0.5,0.75,1]
 	M = np.flip(M,axis=0)
 	names = ['Max Aspect Ratio','Max Mesh Non-Orthogonality','Average Mesh Non-Orthogonality','Max Skewness']
-	fig,axs = plt.subplots(2,2,figsize=(8,4.5))
-	plt.subplots_adjust(wspace=0.25,left=0.1,right=0.975,bottom=0.1,top=0.925,hspace=0.5)
+	fig,axs = plt.subplots(2,2,figsize=(8,4))
+	plt.subplots_adjust(wspace=0.15,left=0.08,right=0.99,bottom=0.12,top=0.925,hspace=0.6)
 	for i, ax in zip(range(len(M)), axs.ravel()):
-		ax.imshow(M[i].T)
+		im = ax.imshow(M[i].T)
 		ax.set_title(names[i])
+		plt.colorbar(im,ax=ax)
 		ax.set_ylabel('Axial Fidelity')
 		ax.set_xlabel('Radial Fidelity')
 		ax.set_xticks(np.arange(10),list(np.arange(11)/10)[1:])
 		ax.set_yticks(np.arange(len(f2)),np.flip(f2))
 	fig.savefig('multi_fidelity/qualities.pdf')
 	return 
-
 
