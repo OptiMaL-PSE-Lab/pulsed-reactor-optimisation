40277b896a2008a7c7c58e64cb50ebffd9e259a2
Tom Savage <trs20@ic.ac.uk>
2022-08-15 15:51:47 +0100

    updated HPC

--- simulation-integration/mesh_validation.py
+++ simulation-integration/mesh_validation.py
@@ -5,47 +5,42 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from matplotlib.pyplot import cm
 
-# exp = pd.read_csv('simulation-integration/output_validation/experiments.csv')
+fig,ax = plt.subplots(1,2,figsize=(8,4))
+fig.subplots_adjust(wspace=0.2,bottom=0.15,right=0.975,left=0.075)
+for i in range(2):
+    ax[i].set_xlabel(r'$\theta$')
+    ax[i].set_ylabel(r'$E({\theta})$')
+    ax[i].set_ylim(-0.1,2.1)
+    ax[i].set_xlim(0,2)
 
-# t1 = exp['t1'][exp['t1'].isnull()==False].values
-# t2 = exp['t2'][exp['t2'].isnull()==False].values
-# e1 = exp['e1'][exp['e1'].isnull()==False].values
-# e2 = exp['e2'][exp['e2'].isnull()==False].values
+ax[0].scatter(t1,e1,c='k',s=10,marker='x',lw=0.75,zorder=10)
+ax[1].scatter(t2,e2,c='k',s=10,marker='x',lw=0.75,label='Experiment',zorder=10)
+ax[0].set_title('Experiment 1')
+ax[1].set_title('Experiment 2')
 
-# fig,ax = plt.subplots(1,2,figsize=(6,3))
-# fig.subplots_adjust(wspace=0.3,bottom=0.2,right=0.95)
-# for i in range(2):
-#     ax[i].set_xlabel(r'$\theta$')
-#     ax[i].set_ylabel(r'$E({\theta})$')
-#     ax[i].set_ylim(-0.1,2.5)
-#     ax[i].set_xlim(0,2.5)
+f1i = [0,0.25,0.5,0.75,1]
+f2i = [0,0.25,0.5,0.75,1]
+color = iter(cm.coolwarm(np.linspace(0, 1, 5)))
 
-# ax[0].scatter(t1,e1,c='k',s=10,marker='x',lw=0.5)
-# ax[1].scatter(t2,e2,c='k',s=10,marker='x',lw=0.5,label='Experiment')
-
-
-color = iter(cm.viridis_r(np.linspace(0, 1, 3)))
-
-f1i = [0,1]
-f2i = [1,0]
 for i in range(len(f1i)):
 
     f1 = f1i[i]
     f2 = f2i[i]
 
-    #c = next(color)
+    c = next(color)
     N,time,value,path = eval_cfd_validation(0.002,5,50,0.012,0.01,0.0025,0.0753,[f1,f2])
     theta,etheta = val_to_rtd(time,value,path)
 
     # df = pd.read_csv('simulation-integration/output_validation/e_1_fid_'+str(f)+'.csv')
     # theta = df['theta'].values
     # etheta = df['etheta'].values
-    # ax[0].plot(theta,etheta,c=c)
+    ax[0].plot(theta,etheta,c=c)
 
     res1 = pd.DataFrame({'time':time,'concentration':value})
     res2 = pd.DataFrame({'theta':theta,'etheta':etheta})
     res = pd.concat([res1, res2], axis=1) 
     res.to_csv('simulation-integration/output_validation/e_1_fid_'+str(f1)+'_'+str(f2)+'.csv')
+    fig.savefig('simulation-integration/output_validation/experimental_validation.pdf')
 
     N,time,value,path = eval_cfd_validation(0.004,5,50,0.012,0.01,0.0025,0.0753,[f1,f2])
     theta,etheta = val_to_rtd(time,value,path)
@@ -53,13 +48,15 @@ for i in range(len(f1i)):
     # df = pd.read_csv('simulation-integration/output_validation/e_2_fid_'+str(f)+'.csv')
     # theta = df['theta'].values
     # etheta = df['etheta'].values
-    # ax[1].plot(theta,etheta,c=c,label='Fidelity: '+str(f))
-    # ax[0].grid(alpha=0.5)
-    # ax[1].grid(alpha=0.5)
+    ax[1].plot(theta,etheta,c=c,label='Fidelity: '+str(i+1))
+    ax[0].grid(alpha=0.5)
+    ax[1].grid(alpha=0.5)
     res1 = pd.DataFrame({'time':time,'concentration':value})
     res2 = pd.DataFrame({'theta':theta,'etheta':etheta})
     res = pd.concat([res1, res2], axis=1) 
     res.to_csv('simulation-integration/output_validation/e_2_fid_'+str(f1)+'_'+str(f2)+'.csv')
+    fig.savefig('simulation-integration/output_validation/experimental_validation.pdf')
 
-# ax[1].legend()
-# fig.savefig('simulation-integration/output_validation/experimental_validation.pdf')
+ax[1].legend(frameon=False)
+fig.savefig('simulation-integration/output_validation/experimental_validation.pdf')
+
