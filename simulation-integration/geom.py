843716ff0cf3714214e183ef6d46a78d75c4e78a
Tom Savage <trs20@ic.ac.uk>
2022-08-25 08:58:12 +0100

    refactoring of main code

--- simulation-integration/geom.py
+++ simulation-integration/geom.py
@@ -1,30 +1,35 @@
 from bayes_opt_with_constraints.bayes_opt import BayesianOptimization, UtilityFunction
-from utils import newJSONLogger
+from datetime import datetime
+from utils import newJSONLogger, vel_calc, parse_conditions, run_cfd, calculate_N
 from bayes_opt_with_constraints.bayes_opt.event import Events
 import json
-from datetime import datetime
 import os 
 import numpy as np 
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+from mesh_generation.coil_basic import create_mesh
 
-logger = newJSONLogger(path="outputs/geom/logs.json")
-
-# defining utility function
-utility = UtilityFunction(kind="ucb", kappa=2.5, xi=0.0)
-
-def eval_cfd(a, f, re, coil_rad, pitch):
-    tube_rad = 0.0025
-    length = 0.0785
+def eval_cfd(a, f, re, coil_rad, pitch, inversion_loc):
     fid = [1,1]
-    inversion_loc = None
+    tube_rad = 0.0025
+    length = 0.0753
     identifier = datetime.now().strftime("%m_%d_%Y__%H_%M_%S")
     print('Starting to mesh '+identifier)
     newcase = "outputs/geom/" + identifier
-    create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, fid,path=newcase,validation=False,build=True)
+    create_mesh(coil_rad, tube_rad, pitch, length, inversion_loc, fid,path=newcase,validation=True,build=True)
     vel = vel_calc(re)
     parse_conditions(newcase, a, f, vel)
     time, value = run_cfd(newcase)
     N = calculate_N(value, time,newcase)
-    return N
+    return N,time,value,newcase
+
+logger = newJSONLogger(path="outputs/geom/logs.json")
+
+# defining utility function
+utility = UtilityFunction(kind="ucb", kappa=5, xi=0.0)
+
+
+
 
 # setting up the optimisation problem
 optimizer = BayesianOptimization(
@@ -35,6 +40,7 @@ optimizer = BayesianOptimization(
         "re": (10, 50),
         "coil_rad": (0.003, 0.0125),
         "pitch": (0.0075, 0.015),
+        "inversion_loc": (0,1)
     },
     pcons=[],
     verbose=2,
@@ -55,22 +61,25 @@ except FileNotFoundError:
     pass
 # assign logger to optimizer
 optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
-lb = np.array([0.001,2,10,0.003,0.0075])
-ub = np.array([0.008,8,50,0.0125,0.015])
+
+
+iteration = 0
+lb = np.array([0.001,2,10,0.003,0.0075,0])
+ub = np.array([0.008,8,50,0.0125,0.015,1])
 n_init = 15
 init_points = np.random.uniform(0,1,(len(lb),n_init))
 init_points = np.array([[(init_points[i,j] + lb[i]) *(ub[i]-lb[i]) for i in range(len(lb))]for j in range(n_init)])
 
-keys = ['a','f','re','coil_rad','pitch']
+keys = ['a','f','re','coil_rad','pitch','inversion_loc']
 for p in init_points:
     p_dict = {}
     for i in range(len(keys)):
         p_dict[keys[i]] = p[i]
     target = eval_cfd(**p_dict)
     optimizer.register(params=p_dict, target=target)
+    iteration += 1
 
 while True:
     next_point = optimizer.suggest(utility)
-    print(next_point)
     target = eval_cfd(**next_point)
     optimizer.register(params=next_point, target=target)
