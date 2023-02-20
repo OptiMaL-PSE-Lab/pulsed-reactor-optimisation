e2a1ac3ac3992b1f5961cba160677aa82ad608b3
Tom Savage <trs20@ic.ac.uk>
2023-02-20 09:35:16 +0000

    update all runs

--- pulsed_flow_helical_tube/run.sh
+++ pulsed_flow_helical_tube/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#PBS -N pulsed_flow_helical_tube
+#PBS -N pulsed_flow_helical_tube_third
 #PBS -j oe
-#PBS -o pulsed_flow_helical_tube/logs.out
-#PBS -e pulsed_flow_helical_tube/logs.err
+#PBS -o pulsed_flow_helical_tube/third_run/logs.out
+#PBS -e pulsed_flow_helical_tube/third_run/logs.err
 #PBS -lselect=1:ncpus=48:mem=32gb
 #PBS -lwalltime=72:00:00
 
