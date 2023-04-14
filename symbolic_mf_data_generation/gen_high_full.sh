b6e48f047a5f12108f9f82bb805449a603e32cce
Tom Savage <t.savage@imperial.ac.uk>
2023-04-14 10:31:41 +0000

    added generation of mf data

---
+++ symbolic_mf_data_generation/gen_high_full.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -N sym_dat
+#PBS -j oe
+#PBS -o symbolic_mf_data_generation/high_full/logs.out
+#PBS -e symbolic_mf_data_generation/high_full/logs.err
+#PBS -lselect=1:ncpus=48:mem=64gb
+#PBS -lwalltime=72:00:00
+
+module load anaconda3/personal
+module load openfoam/1906
+module load intel-suite
+
+cd $PBS_O_WORKDIR
+source activate mf_design_env
+python3 -B symbolic_mf_data_generation/generate_mf_data.py symbolic_mf_data_generation/high_full/data.json $cpus 30 3 100
