66bc5a1ab1900e95112d44234bd49bfef1166bfe
Tom Savage <t.savage@imperial.ac.uk>
2023-06-15 13:39:31 +0000

    finalised U and P extraction

--- emulation/extract_data.py
+++ emulation/extract_data.py
@@ -21,26 +21,34 @@ def extract_U_vals(U):
     new_U = np.array(new_U)
     return new_U
 
+def extract_P_vals(P):
+    P = P[23:]
+    i = 0 
+    while list(str(P[i]).split("""b'""")[-1])[0] != ')':
+        i += 1
+    P_list = P[:i]
+    new_P = []
+    for P in P_list:
+        P = str(P)
+        P = float(P.split("""b'""")[-1].split("""\\n""")[0])
+        new_P.append(P)
+    new_P = np.array(new_P)
+    return new_P
+
 def extract_data(path):
     U_path = path + "/U.gz"
-    p_path = path + "/U.gz"
+    p_path = path + "/p.gz"
 
     with gzip.open(U_path, 'rb') as f:
         U = f.readlines()
     U = extract_U_vals(U)
-    # with gzip.open(p_path, 'rb') as f:
-    #     P = f.readlines()
-    # P = extract_vals(P)
-    return U
+
+    with gzip.open(p_path, 'rb') as f:
+        P = f.readlines()
+    P = extract_P_vals(P)
+    return U,P
 
 
-path = "emulation/simulations/dd9abf2c-4e89-40c9-9a32-025ff5bf3641_r"
-U = extract_data(path)
-# print(U)
-print(len(U))
-
-# plt.figure()
-# for i in range(3):
-#     plt.plot(np.arange(len(U)),U[:,i])
-# plt.show()
-
+path = "emulation/simulations/36958981-e548-44ef-8528-d60c52cb4e94_r"
+U,P = extract_data(path)
+print(U.shape,P.shape)
